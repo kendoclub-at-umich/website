aa969530e9b812bb3848bf6dfd95684ebb23f2ee
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2023-11-04 20:52:12 -0400

    feat: :mag: add canoncial link to prevent google indexing netlify app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<link rel="canonical" href="https://websites.umich.edu/~kendoclub{$page.url.pathname}" />
+</svelte:head>
