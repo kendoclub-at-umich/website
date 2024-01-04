f40a427c026dd2bf508d450b740ab160fbe05f54
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-01-03 22:46:40 -0500

    feat: :sparkles: add section for "what is kendo?"
    What I am adding to the home page on this branch is a combination of excerpts/edits of content on our old kendo website and content authored by me. Credit for the content from the previous website goes to the people who wrote it (there is no record of who is credited for writing what, so I am unable to specify that exactly).

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
 <h1>Welcome to Kendo Club!</h1>
+
+<h1>What is Kendo?</h1>
+Kendo is a Japanese martial art which literally translates to “The Way of the Sword”. It's an athletic
+sport which is played by means of one-on-one striking between opponents using shinai (sword made from
+bamboo slats) while wearing kendo-gu (protective armor).
