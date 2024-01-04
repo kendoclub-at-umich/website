9a1a13ab2911157fde00bc6c0520c720e6decdc4
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-01-04 06:58:44 -0500

    feat: :sparkles: add details about how to join to home page
    This is a first draft, and it is likely too wordy for the home page. It should be edited to be more concise, reworded to be simpler, and some excess information be moved to another part of the website or removed.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,24 @@
 Kendo is a Japanese martial art which literally translates to “The Way of the Sword”. It's an athletic
 sport which is played by means of one-on-one striking between opponents using shinai (sword made from
 bamboo slats) while wearing kendo-gu (protective armor).
+
+<h1>How do you join?</h1>
+<ul>
+	<li>Come to practice</li>
+	<ul>
+		<li>
+			You can come to any practice session you like (though you may find it easier to join closer to
+			the beginning of a new semester).
+		</li>
+		<li>
+			Please wear comfortable athletic clothes. Bottoms that cover the knee are preferred because
+			there is some kneeling involved. This is a barefoot sport, so please leave your shoes outside
+			the ballroom before entering.
+		</li>
+		<li>Masking is optional. Surgical or cloth masks are the best choices for breathability.</li>
+	</ul>
+	<li>
+		Email us at the email address listed on the "Contact Info" page. We can answer additional
+		questions you have and add you to our email list.
+	</li>
+</ul>
