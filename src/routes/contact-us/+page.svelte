cdfa90e6d20161f550c413170d8f81511a440ce6
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-04 18:58:13 -0400

    feat: :sparkles: add social media icons linking to the pages

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,5 +1,24 @@
+<script>
+	import fb_logo from '$lib/assets/Facebook_Logo_Primary.png';
+	import ig_logo from '$lib/assets/Instagram_Glyph_Gradient.png';
+</script>
+
 <h1>Contact Us</h1>
 
 <p>
 	Email Address: <a href="mailto:kendoinfo@umich.edu"> kendoinfo@umich.edu</a>
 </p>
+
+<h2>Find us on:</h2>
+
+<figure>
+	<a href="https://www.facebook.com/groups/1038072369705184">
+		<img src={fb_logo} alt="Facebook logo" /></a
+	>
+</figure>
+
+<figure>
+	<a href="https://www.instagram.com/kendoumich/">
+		<img src={ig_logo} alt="Instagram logo" />
+	</a>
+</figure>
