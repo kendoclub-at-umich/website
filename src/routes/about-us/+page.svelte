2665fa43564380493b0db0e3de7e262e12be7015
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-04 19:56:22 -0400

    feat: :sparkles: copy content from old kendo website

---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,38 @@
+<h1>About Us</h1>
+
+<h2>History</h2>
+<p>
+	The Kendo Club at the University of Michigan was first started in the Fall of 2001 and officially
+	recognized by the Midwest Kendo (MWKF) in Fall 2002. Our continuing purpose is to provide an open
+	and inviting opportunity for University of Michigan students and local community members to learn
+	and practice Kendo. Instruction of our club was originally lead by Chika and Takashi Ezaki and
+	transitioned to Yukio Watanabe and Joe Ponchart once they returned to Japan in 2003. After the
+	ground work was laid out for a consistent practice space at the University of Michigan, cross
+	training with the Kendo club at Eastern Michigan University continued and still continues to this
+	day. Our shared history with EMU through Eastern Kendo reaffirms our continued partnership as both
+	clubs seek to advance and promote the practice of Kendo in the Midwest. Many of our instructors
+	and members train at both locations even to this day. Our club is classified as a student
+	organization through the Michigan Student Assembly, the student government of the University of
+	Michigan. Our Kendo Club is also part of the Midwest Kendo Federation (MWKF) which is the regional
+	chapter under the All United States Kendo Federation (AUSKF), the national governing body for
+	Kendo in this country. The MWKF is responsible for all Kendo clubs and Dojos in the Midwest area.
+</p>
+
+<h2>Motto</h2>
+<p>
+	Our club motto reads "Shin Gi Ittai", which translates approximately to "The Spirit and Skill are
+	One Body" or "The Heart and Technique are Together". This motto was one of several proposed by
+	Masaki Hori in the fall of 2004, and was chosen by our club officers and coaches to best represent
+	the ideals we strive for in our club. The motto was brushed by Cheng Xin Liu (b. 1943) for our
+	club and subsequently updated by Wan Li Hsu for screen print use. This motto is on our club
+	tenugui, which is traditionally given to club members before their first competition as a sign of
+	mutual support and encouragement.
+</p>
+
+<h2>Constitution</h2>
+<p>
+	As required by the University of Michigan and the SOAR (Student Organization and Recognition)
+	Initiative, a copy of our club constitution can be found <a
+		href="https://websites.umich.edu/~kendoclub/resources/constitution.pdf">here</a
+	>.
+</p>
