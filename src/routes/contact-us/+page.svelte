df4fc73a22d555a559a3c2a21fcdfb6fa339d46f
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-04 17:32:16 -0400

    feat: :art: put the text in a p tag

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,3 +1,5 @@
 <h1>Contact Us</h1>
 
-Email Address:<a href="mailto:kendoinfo@umich.edu"> kendoinfo@umich.edu</a>
+<p>
+	Email Address: <a href="mailto:kendoinfo@umich.edu"> kendoinfo@umich.edu</a>
+</p>
