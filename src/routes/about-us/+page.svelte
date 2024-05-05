305458bb0211571d877ad3f3a0e36dd47f2f9c4f
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-04 20:25:29 -0400

    feat: :sparkles: add back in hyperlinks from old website

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -2,20 +2,11 @@
 
 <h2>History</h2>
 <p>
-	The Kendo Club at the University of Michigan was first started in the Fall of 2001 and officially
-	recognized by the Midwest Kendo (MWKF) in Fall 2002. Our continuing purpose is to provide an open
-	and inviting opportunity for University of Michigan students and local community members to learn
-	and practice Kendo. Instruction of our club was originally lead by Chika and Takashi Ezaki and
-	transitioned to Yukio Watanabe and Joe Ponchart once they returned to Japan in 2003. After the
-	ground work was laid out for a consistent practice space at the University of Michigan, cross
-	training with the Kendo club at Eastern Michigan University continued and still continues to this
-	day. Our shared history with EMU through Eastern Kendo reaffirms our continued partnership as both
-	clubs seek to advance and promote the practice of Kendo in the Midwest. Many of our instructors
-	and members train at both locations even to this day. Our club is classified as a student
-	organization through the Michigan Student Assembly, the student government of the University of
-	Michigan. Our Kendo Club is also part of the Midwest Kendo Federation (MWKF) which is the regional
-	chapter under the All United States Kendo Federation (AUSKF), the national governing body for
-	Kendo in this country. The MWKF is responsible for all Kendo clubs and Dojos in the Midwest area.
+	<a href="https://www.midwestkendofederation.org/">Midwest Kendo Federation (MWKF)</a>
+	which is the regional chapter under the
+	<a href="https://www.auskf.org/">All United States Kendo Federation (AUSKF)</a>, the national
+	governing body for Kendo in this country. The MWKF is responsible for all Kendo clubs and Dojos in
+	the Midwest area.
 </p>
 
 <h2>Motto</h2>
@@ -32,7 +23,8 @@
 <h2>Constitution</h2>
 <p>
 	As required by the University of Michigan and the SOAR (Student Organization and Recognition)
-	Initiative, a copy of our club constitution can be found <a
-		href="https://websites.umich.edu/~kendoclub/resources/constitution.pdf">here</a
+	Initiative,
+	<a href="https://websites.umich.edu/~kendoclub/resources/constitution.pdf">
+		a copy of our club constitution can be found here</a
 	>.
 </p>
