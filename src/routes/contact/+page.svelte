5e58e970d6d50441610fd35db7b9caf3c7f235e7
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-07 18:29:11 -0400

    feat: :fire: remove social media links after considering values and possible alternate locations
    Had a value discussion considering why the social media links would be on the website and what value they'd bring to the club. Best solution: Relocate the Facebook group link to another location more directly associated with members as opposed to the general public. Instagram doesn't need to be linked for now.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,23 +8,3 @@
 <p>
 	Email Address: <a href="mailto:kendoinfo@umich.edu"> kendoinfo@umich.edu</a>
 </p>
-
-<h2>Find us on:</h2>
-
-<a href="https://www.facebook.com/groups/1038072369705184">
-	<img class="social-media" src={fbLogo} alt="Facebook" /> KendoClub@Michigan
-</a>
-<a href="https://www.instagram.com/kendoumich/">
-	<img class="social-media" src={igLogo} alt="Instagram" /> @kendoumich
-</a>
-
-<style>
-	.social-media {
-		--icon-width: 75px;
-
-		box-sizing: content-box;
-		width: var(--icon-width);
-		height: auto;
-		padding: calc(var(--icon-width) / 4);
-	}
-</style>
