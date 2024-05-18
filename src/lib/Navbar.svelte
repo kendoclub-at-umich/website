b9abe4e6a7a04570fd1354b06f7d8c97b62a8b4e
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-17 22:46:14 -0400

    feat: :sparkles: move navbar into its own file
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	export let logoUrl: string;
+	export let siteName: string;
+	type Page = { name: string; url: string };
+	export let pages: readonly Page[];
+</script>
+
+<header>
+	<div class="container">
+		<nav>
+			<ul>
+				<li>
+					<a href="/" class="brand contrast"><img src={logoUrl} alt="" />{siteName}</a>
+				</li>
+			</ul>
+			<ul>
+				{#each pages as page}
+					<li><a href={page.url}>{page.name}</a></li>
+				{/each}
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<style>
+	header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		background-color: color-mix(in lab, var(--pico-background-color) 90%, transparent);
+		backdrop-filter: blur(12px);
+		box-shadow:
+			0 3px 6px rgba(0, 0, 0, 0.16),
+			0 3px 6px rgba(0, 0, 0, 0.23);
+	}
+
+	.brand {
+		display: grid;
+		grid-template-columns: auto auto;
+		align-items: center;
+		gap: 8px;
+		font-size: 1.5em;
+		font-weight: 700;
+
+		& > img {
+			height: 1lh;
+		}
+	}
+
+	header + :global(*) {
+		margin-top: calc(var(--font-size) * 2.25 + var(--nav-element-spacing-vertical) * 2);
+		padding-top: var(--font-size);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,62 +1,22 @@
-<script>
+<script lang="ts">
 	import '$lib/styles.scss';
 	import { page } from '$app/stores';
-	import logo from './logo.webp';
+	import Navbar from '$lib/Navbar.svelte';
+	import logoUrl from './logo.webp';
+
+	const pages = [
+		{ name: 'Calendar', url: '/calendar' },
+		{ name: 'About', url: '/about' },
+		{ name: 'Contact Us', url: '/contact' }
+	] as const;
 </script>
 
 <svelte:head>
 	<link rel="canonical" href="https://websites.umich.edu/~kendoclub{$page.url.pathname}" />
 </svelte:head>
 
-<header>
-	<div class="container">
-		<nav>
-			<ul>
-				<li>
-					<a href="/" class="brand contrast"><img src={logo} alt="" />Kendo Club at Umich</a>
-				</li>
-			</ul>
-			<ul>
-				<li><a href="/calendar">Calendar</a></li>
-				<li><a href="/about">About</a></li>
-				<li><a href="/contact">Contact Us</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
+<Navbar {logoUrl} siteName="Kendo Club at Umich" {pages} />
 
 <main class="container">
 	<slot />
 </main>
-
-<style>
-	header {
-		position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-		background-color: color-mix(in lab, var(--pico-background-color) 90%, transparent);
-		backdrop-filter: blur(12px);
-		box-shadow:
-			0 3px 6px rgba(0, 0, 0, 0.16),
-			0 3px 6px rgba(0, 0, 0, 0.23);
-	}
-
-	.brand {
-		display: grid;
-		grid-template-columns: auto auto;
-		align-items: center;
-		gap: 8px;
-		font-size: 1.5em;
-		font-weight: 700;
-
-		& > img {
-			height: 1lh;
-		}
-	}
-
-	main {
-		margin-top: calc(var(--font-size) * 2.25 + var(--nav-element-spacing-vertical) * 2);
-		padding-top: var(--font-size);
-	}
-</style>
