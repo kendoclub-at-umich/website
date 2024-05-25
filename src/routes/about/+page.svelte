c04abd6515d66e962a727a8c7eaa8a3c564f090b
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-25 17:08:58 -0400

    feat: :sparkles: add link to Seishinkan Kendo

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,10 +6,11 @@
 	by the Midwest Kendo Federation (MWKF) in the fall of 2002. Instruction of our club was originally
 	lead by Chika and Takashi Ezaki. Once they returned to Japan in 2003, it transitioned to Yukio
 	Watanabe and Joe Ponchart. After the groundwork was laid out for a consistent practice space at
-	the University of Michigan, cross training with Seishinkan Kendo in Ypsilanti (formerly Eastern
-	Kendo Club) continued. Our shared history reaffirms our continued partnership as both clubs seek
-	to advance and promote the practice of Kendo in the Midwest. Many of our instructors and members
-	train at both locations even to this day.
+	the University of Michigan, cross training with
+	<a href="https://www.seishinkankendo.com/">Seishinkan Kendo</a> in Ypsilanti (formerly Eastern Kendo
+	Club) continued. Our shared history reaffirms our continued partnership as both clubs seek to advance
+	and promote the practice of Kendo in the Midwest. Many of our instructors and members train at both
+	locations even to this day.
 </p>
 <p>
 	Our club is classified as a student organization through the Michigan Student Assembly, the
