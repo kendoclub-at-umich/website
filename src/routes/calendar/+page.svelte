3cd7dbf87b244e2365a2266f54cf83a3b31c362c
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-05-25 13:51:43 -0400

    fix: 📱 make calendar look good on all screen sizes

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -7,3 +7,17 @@
 </script>
 
 <div id="calendar-container" />
+
+<style>
+	:global(main:has(#calendar-container)) {
+		padding-inline: var(--pico-spacing);
+		width: auto;
+		max-width: none;
+	}
+
+	#calendar-container {
+		margin: 0 auto;
+		font-size: min(18px, 0.75em);
+		max-width: max(640px, calc(400vh / 3 - 270px));
+	}
+</style>
