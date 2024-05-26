b03151e39ceb18f3b3b88aef1d38656860cde4a3
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-05-26 19:47:12 -0400

    fix: :bug: make calendar render every time
    full-calendar.ts was being imported every time, but the code inside it was only running the time it was being initialized (the first time it was imported) because that's how the module system in js works. In order to fix that, we put the code to create and render the calendar in a function and exported it, and we have it run after every import.
    
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	onMount(() => {
-		import('./full-calendar');
+	onMount(async () => {
+		const module = await import('./full-calendar');
+		module.generateCalendar();
 	});
 </script>
 
--- src/routes/calendar/full-calendar.ts
+++ src/routes/calendar/full-calendar.ts
@@ -6,28 +6,30 @@ import css from './full-calendar.css?inline';
 
 const smallScreenQuery = matchMedia('(width < 768px)');
 
-const calendarContainer = document.querySelector('#calendar-container')!;
+export function generateCalendar() {
+	const calendarContainer = document.querySelector('#calendar-container')!;
 
-const shadowDom = calendarContainer.attachShadow({ mode: 'open' });
+	const shadowDom = calendarContainer.attachShadow({ mode: 'open' });
 
-const styleSheet = new CSSStyleSheet();
-styleSheet.replaceSync(css);
-shadowDom.adoptedStyleSheets = [styleSheet];
+	const styleSheet = new CSSStyleSheet();
+	styleSheet.replaceSync(css);
+	shadowDom.adoptedStyleSheets = [styleSheet];
 
-const calendarElement = document.createElement('div');
-shadowDom.appendChild(calendarElement);
+	const calendarElement = document.createElement('div');
+	shadowDom.appendChild(calendarElement);
 
-const calendar = new Calendar(calendarElement, {
-	plugins: [dayGridPlugin, listPlugin, googleCalendarPlugin],
-	initialView: smallScreenQuery.matches ? 'listMonth' : 'dayGridMonth',
-	headerToolbar: {
-		left: 'prev,next today',
-		center: 'title',
-		right: 'dayGridMonth,listMonth'
-	},
-	googleCalendarApiKey: 'AIzaSyDHUqblOxNndbN7jUXvoKy8IugKLlSXbkE',
-	events: {
-		googleCalendarId: 'c_1pcp6odi9qfe276tpuob8h00ms@group.calendar.google.com'
-	}
-});
-calendar.render();
+	const calendar = new Calendar(calendarElement, {
+		plugins: [dayGridPlugin, listPlugin, googleCalendarPlugin],
+		initialView: smallScreenQuery.matches ? 'listMonth' : 'dayGridMonth',
+		headerToolbar: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'dayGridMonth,listMonth'
+		},
+		googleCalendarApiKey: 'AIzaSyDHUqblOxNndbN7jUXvoKy8IugKLlSXbkE',
+		events: {
+			googleCalendarId: 'c_1pcp6odi9qfe276tpuob8h00ms@group.calendar.google.com'
+		}
+	});
+	calendar.render();
+}
