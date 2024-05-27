7b4dc15fbac89fa7911c723c34dcd61c9b855701
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-05-27 11:20:46 -0400

    refactor: ♻️ refine copyright regex

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import { readFileSync } from 'node:fs';
 
 const license = readFileSync('./LICENSE', { encoding: 'utf8' });
-const copyright = /Copyright \(c\) ([0-9]{4} [A-Za-z ]+)/.exec(license)![1];
+const copyright = /Copyright \(c\) ([0-9]{4}( [A-Z]?[a-z]+)+)/.exec(license)![1];
 
 export default defineConfig({
 	plugins: [sveltekit()],
