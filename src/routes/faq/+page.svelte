070d2630be2d066003e95c534e221db7292e1bba
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-07-04 16:17:25 -0400

    feat: :sparkles: make grammar, spelling, and conciseness edits
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -7,72 +7,44 @@
 <hr />
 
 <details>
-	<summary>It looks dangerous, will I get hurt or injured?</summary>
+	<summary>This looks dangerous. Will I get hurt or injured?</summary>
 	<p>
-		As with any sports, there is some level of risk involved. But in general, injuries are quite
-		rare for Kendo players. Our bamboo sword (shinai) and armor (bogu) are designed to absorb
-		impact, and have been perfected for over 300 years. If your opponent misses the correct target,
-		it can sting a little.
+		As with any sport, there is some level of risk involved. However, injuries are quite rare in
+		Kendo. The bamboo swords (shinai) and armor (bogu) are designed to absorb impact and protect
+		you.
 	</p>
 	<p>
 		Before you put on bogu, we will teach you how to protect yourself and others from accidental
-		harm. It is an important part of Kendo to fight with respect and gratitude instead of with
-		muscle and anger, so Kendo players always put their opponents’ safety prior to winning a
-		sparing.
+		harm. It is an important part of Kendo to fight with respect and gratitude rather than force and
+		anger, so Kendo players always put their opponents’ safety above winning a sparring match.
+		Safety routines in kendo training include regular inspection of shinai and bogu and proper
+		stretching before practice.
 	</p>
 	<p>
-		Safety routine in kendo training includes regular inspection of shinai and bogu, proper
-		stretching before practice, etc.
+		The most common injury is your opponent missing the correct target, which can sting a little and
+		possibly bruise.
 	</p>
 </details>
 
 <hr />
 
 <details>
-	<summary>Is Kendo an expensive hobby?</summary>
-	<p>In general, the cost of practicing Kendo includes:</p>
-	<ol>
-		<li>Club dues and gym pass (see home page).</li>
-		<li>
-			AUSKF membership (<a href="https://www.auskf.org/">https://www.auskf.org/</a>):<br />
-			The AUSKF (All United States Kendo Federation) membership is ~ $80 per year. Students get discount.
-			Beginners do not need to pay until putting on bogu.
-		</li>
-		<li>
-			Promotion test fees and tournament fees:<br />
-			As a reference, in 2024, for Kyu test it’s around $40 (test) + $30 (certificate); for 1-Dan (black
-			belt entry level) it’s around $50 (test) + $50 (certificate). It goes higher for higher ranks.
-			Tournament registration fee varies, typically $30 - $70.
-		</li>
-		<li>
-			Shinai (bamboo sword) and bokuto (wooden sword):<br />
-			Depending on practice frequency, a shinai ($30 - $60) can last several months (4-12 month). There
-			are ways to reduce the cost by combining orders to get discounts, or making franken-shinai with
-			the intact slats from broken shinais. Bokken (~ $50 and up) is only need for promotion test and
-			typically never breaks.
-		</li>
-		<li>
-			Bogu (armor):<br />
-			Bogu is probably <strong>the most expensive component</strong>, but it can last decades with
-			good maintenance. A good beginner set can be ~ $400 and up. Always ask around as our
-			instructors and senior members might have discount information for some vendors. Also, our
-			club might have loaner bogu sets for rent.<br />
-			<br />
-			Beginners will not wear bogu until they have learned some solid fundamentals, so you don’t need
-			to worry about this in the beginning.
-		</li>
-	</ol>
+	<summary>How much does Kendo cost?</summary>
+	<p>
+		Costs vary greatly from person to person. Most beginners spend around $200 per year. Around a
+		year in, most club members spend about $500 on their first set of bogu (armor). Some advanced
+		members spend upwards of $300 a year.
+	</p>
 </details>
 
 <hr />
 
 <details>
-	<summary>Where do I buy a bogu set and how do I find the right size of it?</summary>
+	<summary>Where do I buy a bogu set and how do I find the right size?</summary>
 	<p>
-		Consult the instructors first (you need their approval for wearing bogu anyway), they will help
-		you with the measurements and direct you to some bogu vendors that we have had good experience
-		with. Depending on the order, the club might have shortcode discount or could arrange group
-		orders.
+		Consult the instructors first (you need their approval for wearing bogu anyway). They will help
+		you with measurements and direct you to bogu vendors we have had good experiences with.
+		Depending on the order, the club might have a shortcode discount or could arrange group orders.
 	</p>
 </details>
 
@@ -81,9 +53,9 @@
 <details>
 	<summary>Do you use real swords?</summary>
 	<p>
-		No. Kendo uses 2 types of swords: (1) the bamboo sword (shinai) for general practice and
-		competition, (2) the wooden sword (bokuto) to more closely approximate the shape and weight of a
-		sword when practicing forms (Kata).
+		No. Kendo uses 2 types of swords: the bamboo sword (shinai) for general practices and
+		competitions, and the wooden sword (bokuto) to more closely approximate the shape and weight of
+		a sword when practicing forms (Kata).
 	</p>
 	<p>
 		During some Kata demonstrations we may use metal swords, but because of the possibility of
@@ -97,9 +69,8 @@
 	<summary>Do you go to competitions/tournaments?</summary>
 	<p>
 		Yes. Kendo is a martial art with an active competition component, and tournament is one of the
-		best ways to meet Kendo friends. Our club members compete in 3-4 tournaments per year in local
-		areas. Our awesome student officers also organize an internal tournament within our club every
-		year!
+		best ways to meet new friends. Our club competes in 3-4 tournaments per year in local areas. Our
+		awesome student officers also organize an internal tournament within our club every year!
 	</p>
 </details>
 
@@ -126,41 +97,40 @@
 <hr />
 
 <details>
-	<summary>
-		How long will it take to earn a black belt and beyond? How to get rank in Kendo?
-	</summary>
+	<summary> How do you increase your rank? </summary>
 	<p>
-		Kendo uses a Kyu/Dan ranking system. Adult Kendo leaners start from 2 Kyu or 1 Kyu, then advance
-		to Dan ranks. Although Dan ranks are considered “black belts”, we do not wear belts or any other
-		outward sign of ranking.
+		Kendo uses a Kyu/Dan ranking system. Adult Kendo learners start from 2-Kyu or 1-Kyu, then
+		advance to Dan ranks. Although Dan ranks are considered “black belts”, we do not wear belts or
+		any other outward sign of ranking.
 	</p>
 	<p>
-		The time to get ranks depending on your dedication and availability. The first stage is to
+		How long it takes to progress depends on your dedication and availability. The first stage is to
 		develop fundamentals, which may take weeks or months. The second stage is to put on bogu and get
 		used to it. Once you are able to perform good fundamentals in bogu, you will get permission from
 		our instructors to test for Kyu. The third stage is to perfect your fundamentals, learn advance
 		techniques, and work towards your first Dan rank.
 	</p>
 	<p>
-		It commonly takes 1-2 semesters to put on bogu, no less than 2 years of regular practice to
-		reach 1 Dan. There is a mandatory waiting period for dan ranks that increases for each new
-		level. 1 Dan to 2 Dan is a 1 year wait, 2 Dan to 3 Dan is a 2 year wait, 3 Dan to 4 Dan is a 3
-		year wait and so on.
+		It commonly takes 1-2 semesters to be ready to start wearing bogu, and no less than 2 years of
+		regular practice to reach 1-Dan (the first "black belt" rank). There is a mandatory waiting
+		period to take the test for the next Dan rank that increases at each subsequent rank. 1-Dan to
+		2-Dan is a 1 year wait, 2-Dan to 3-Dan is a 2 year wait, 3-Dan to 4-Dan is a 3 year wait, and so
+		on.
 	</p>
 </details>
 
 <hr />
 
 <details>
-	<summary>I have no experience, nor am I really physically fit. Will that be a problem?</summary>
+	<summary>I have no experience, nor am I physically fit. Will that be a problem?</summary>
 	<p>
-		It won’t be a problem. Kendo does not have any distinctions between young or old, men or women,
-		athletic or not. As a university club, we are especially dedicated to training beginners.
+		No, it won’t be a problem. Kendo does not have any distinctions between young or old, men or
+		women, athletic or not. As a university club, we are especially dedicated to training beginners.
 	</p>
 	<p>
 		Our practices can be challenging, but they will always also be self-paced for safety. Keep an
-		open mind as you will find your physical limitations challenged, but you will decide how far you
-		wish to push you own limits.
+		open mind, as you will find your physical limitations challenged. You decide how far you wish to
+		push your own limits.
 	</p>
 </details>
 
@@ -171,8 +141,8 @@
 		I have a high ranking belt in another martial art, can it be transferred to Kendo?
 	</summary>
 	<p>
-		Although many martial arts share similar fundamentals, you will start at a similar introduction
-		level in Kendo. Your training in other martial arts might help you advance faster in Kendo.
+		We start everyone with the same basic training in Kendo to build strong fundamentals. However,
+		prior martial arts experience might help you advance faster in Kendo.
 	</p>
 	<p>
 		If you come from a Kumdo background, you may be considered to hold a comparable rank in Kendo.
