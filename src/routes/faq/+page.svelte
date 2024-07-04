c30d43ab4cc124c94a34a88ac8e4c973db9e6bd2
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-07-04 18:11:00 -0400

    feat: :sparkles: change non-proper nouns to lower case, small numbers to a word, and improve conciseness
    Co-authored-by: Heather Stringham <hmstringham@users.noreply.github.com>

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -31,9 +31,10 @@
 <details>
 	<summary>How much does Kendo cost?</summary>
 	<p>
-		Costs vary greatly from person to person. Most beginners spend around $200 per year. Around a
-		year in, most club members spend about $500 on their first set of bogu (armor). Some advanced
-		members spend upwards of $300 a year.
+		Costs vary greatly from person to person. Most beginners spend around $200 per year. After about
+		one year of practice, most club members spend about $500 on their first set of bogu (armor).
+		Some advanced members spend upwards of $300 a year on competing in tournaments and testing for
+		rank.
 	</p>
 </details>
 
@@ -44,7 +45,7 @@
 	<p>
 		Consult the instructors first (you need their approval for wearing bogu anyway). They will help
 		you with measurements and direct you to bogu vendors we have had good experiences with.
-		Depending on the order, the club might have a shortcode discount or could arrange group orders.
+		Depending on the order, the club might have a shortcode discount or could arrange a group order.
 	</p>
 </details>
 
@@ -53,12 +54,12 @@
 <details>
 	<summary>Do you use real swords?</summary>
 	<p>
-		No. Kendo uses 2 types of swords: the bamboo sword (shinai) for general practices and
-		competitions, and the wooden sword (bokuto) to more closely approximate the shape and weight of
-		a sword when practicing forms (Kata).
+		No. Kendo uses two types of swords: the bamboo sword (shinai) for general practices and
+		competitions, and the wooden sword (bokuto) for practicing forms (kata) to more closely
+		approximate the shape and weight of a metal sword.
 	</p>
 	<p>
-		During some Kata demonstrations we may use metal swords, but because of the possibility of
+		During some kata demonstrations we may use metal swords, but because of the possibility of
 		injury this is not common.
 	</p>
 </details>
@@ -68,9 +69,9 @@
 <details>
 	<summary>Do you go to competitions/tournaments?</summary>
 	<p>
-		Yes. Kendo is a martial art with an active competition component, and tournament is one of the
-		best ways to meet new friends. Our club competes in 3-4 tournaments per year in local areas. Our
-		awesome student officers also organize an internal tournament within our club every year!
+		Yes. Kendo is a martial art with an active competition component, and tournaments are one of the
+		best ways to meet new friends. Our club competes in 3-4 tournaments per year. Our awesome
+		student officers also organize an internal tournament within our club every year!
 	</p>
 </details>
 
@@ -99,23 +100,23 @@
 <details>
 	<summary> How do you increase your rank? </summary>
 	<p>
-		Kendo uses a Kyu/Dan ranking system. Adult Kendo learners start from 2-Kyu or 1-Kyu, then
+		Kendo uses a kyu/dan ranking system. Adult Kendo learners start from 2-Kyu or 1-Kyu, then
 		advance to Dan ranks. Although Dan ranks are considered “black belts”, we do not wear belts or
 		any other outward sign of ranking.
 	</p>
 	<p>
 		How long it takes to progress depends on your dedication and availability. The first stage is to
-		develop fundamentals, which may take weeks or months. The second stage is to put on bogu and get
-		used to it. Once you are able to perform good fundamentals in bogu, you will get permission from
-		our instructors to test for Kyu. The third stage is to perfect your fundamentals, learn advance
-		techniques, and work towards your first Dan rank.
+		develop fundamentals, which may take weeks or months. The second stage is to put on bogu (armor)
+		and get used to it. Once you are able to perform good fundamentals in bogu, you will get
+		permission from our instructors to test for Kyu. The third stage is to perfect your
+		fundamentals, learn advanced techniques, and work towards your first dan rank.
 	</p>
 	<p>
-		It commonly takes 1-2 semesters to be ready to start wearing bogu, and no less than 2 years of
+		It commonly takes 1-2 semesters to be ready to start wearing bogu, and no less than two years of
 		regular practice to reach 1-Dan (the first "black belt" rank). There is a mandatory waiting
-		period to take the test for the next Dan rank that increases at each subsequent rank. 1-Dan to
-		2-Dan is a 1 year wait, 2-Dan to 3-Dan is a 2 year wait, 3-Dan to 4-Dan is a 3 year wait, and so
-		on.
+		period to take the test for the next dan rank that increases for each subsequent rank. 1-Dan to
+		2-Dan is a one year wait, 2-Dan to 3-Dan is a two year wait, 3-Dan to 4-Dan is a three year
+		wait, and so on.
 	</p>
 </details>
 
@@ -124,13 +125,14 @@
 <details>
 	<summary>I have no experience, nor am I physically fit. Will that be a problem?</summary>
 	<p>
-		No, it won’t be a problem. Kendo does not have any distinctions between young or old, men or
-		women, athletic or not. As a university club, we are especially dedicated to training beginners.
+		No, it won’t be a problem. Kendo does not have any distinctions between young and old, men and
+		women, athletic and not. As a university club, we are especially dedicated to training
+		beginners.
 	</p>
 	<p>
-		Our practices can be challenging, but they will always also be self-paced for safety. Keep an
-		open mind, as you will find your physical limitations challenged. You decide how far you wish to
-		push your own limits.
+		Our practices can be rigorous, but they will always be self-paced for safety. Keep an open mind,
+		as you will find your physical limitations challenged. You decide how far you wish to push your
+		own limits.
 	</p>
 </details>
 
@@ -138,7 +140,7 @@
 
 <details>
 	<summary>
-		I have a high ranking belt in another martial art, can it be transferred to Kendo?
+		I have a high ranking belt in another martial art. Can it be transferred to Kendo?
 	</summary>
 	<p>
 		We start everyone with the same basic training in Kendo to build strong fundamentals. However,
