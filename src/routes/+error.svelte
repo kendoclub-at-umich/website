e85726c798aa1451b46cbeaec3cea15c64e94d5b
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-07-07 14:30:10 -0400

    feat: :sparkles: make custom error page with font accent color and better size
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page.status} | Kendo Club at the University of Michigan</title>
+</svelte:head>
+
+<h1><span class="primary-color">{$page.status}</span> {$page.error?.message}</h1>
+
+<style>
+	h1 {
+		margin-top: 20svh;
+		text-align: center;
+	}
+
+	.primary-color {
+		color: var(--pico-primary);
+	}
+</style>
