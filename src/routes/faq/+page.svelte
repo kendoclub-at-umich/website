4097d9aed50465a2317288a199f3dab6e4345663
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-07-15 22:29:08 -0400

    add description to FAQ page with call to action

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,4 +1,5 @@
 <svelte:head>
+	<meta name="description" content="Read answers to frequently asked questions about practicing kendo at our club." />
 	<title>FAQ | Kendo Club at the University of Michigan</title>
 </svelte:head>
 
