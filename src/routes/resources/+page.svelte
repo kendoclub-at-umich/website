f114f3dd7e8cc6829290161eb9fd022b60b11971
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-07-20 14:42:24 -0400

    feat: :sparkles: remove clause in resources description that makes it harder to read
    Co-authored-by: Heather Stringham <hmstringham@users.noreply.github.com>

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,9 +1,6 @@
 <svelte:head>
 	<title>Resources | Kendo Club at the University of Michigan</title>
-	<meta
-		name="description"
-		content="Learn more about kendo, from practical tips to philosophy, at any experience level."
-	/>
+	<meta name="description" content="Learn more about kendo, from practical tips to philosophy." />
 </svelte:head>
 
 <h1>Resources</h1>
