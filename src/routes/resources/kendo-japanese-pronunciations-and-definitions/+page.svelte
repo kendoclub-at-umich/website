80b56551ca7aed9c08f90e0adb4f5fbf4b1a1375
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-07-27 14:27:07 -0400

    feat: :sparkles: start converting Kendo Japanese Pronunciation and Definitions into html
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

---
+++ src/routes/resources/kendo-japanese-pronunciations-and-definitions/+page.svelte
@@ -0,0 +1,457 @@
+<svelte:head>
+	<title>
+		Kendo Japanese Pronunciations and Definitions | Kendo Club at the University of Michigan
+	</title>
+	<meta
+		name="description"
+		content="Basic Japanese pronunciation guide and a list of Kendo vocabulary with definitions."
+	/>
+</svelte:head>
+
+<h1>Kendo Japanese Pronunciations and Definitions</h1>
+<p>
+	This document is intended to define and provide a basic pronunciation key for basic Japanese used
+	during kendo practices. The many of our definitions are taken from Jim Breen’s World Wide Web
+	Japanese Online Dictionary, which can be found at
+	<a href="https://www.edrdg.org/cgi-bin/wwwjdic/"> https://www.edrdg.org/cgi-bin/wwwjdic/ </a>, and
+	also from the All U.S. Kendo Federation Website at
+	<a href="https://www.auskf.org/info/what-is-kendo">https://www.auskf.org/info/what-is-kendo</a>.
+	They are great resources.
+</p>
+<p>
+	Disclaimer: We are not professional linguists, so this document may (or most likely) contain
+	errors or information which is not <em>entirely</em> accurate. Please use the following document as
+	a light and entertaining guide to help bridge the language barrier, not as a definitive source.
+</p>
+
+<h2>A few points about the Japanese language</h2>
+<p>
+	Japanese, unlike English, contains many more words which are homonyms, or words that are
+	pronounced the same but have multiple meanings. This is typically a source of confusion for novice
+	Japanese speakers and native Japanese speakers alike. If you try to find definitions of kendo
+	terms on your own, please keep that in mind.
+</p>
+<p>As an example, here are three words pronounced "kendo" with different meanings:</p>
+<dl>
+	<dt>Kendo: 権道 【けんどう】</dt>
+	<dd>inappropriate means to a worthy end; expediency</dd>
+	<dt>Kendo: 県道 【けんどう】</dt>
+	<dd>prefectural road</dd>
+	<dt>Kendo: 剣道 【けんどう】</dt>
+	<dd>kendo; swordsmanship; fencing</dd>
+</dl>
+<p>Here's an amusing non-kendo example:</p>
+<dl>
+	<dt>Niwa niwa niwa niwatori ga imasu: 庭には二羽鶏がいます.</dt>
+	<dd>In the backyard there are two chickens.</dd>
+</dl>
+
+<h2>A few words about pronunciation</h2>
+<p>
+	In general all of the romanized (romaji) versions of Japanese letters are pronounced as written,
+	with one exception. The letter を is written in roman characters as “wo”, as in “kiwotsuke” but is
+	pronounced more often as “o” to sound more like “ki-o-tsuke” or “kyotsuke”, identical to the お
+	“o” sound in “otagai”. It is written in roman characters as “wo” to differentiate the two letters
+	from each other. In some cases the use of “wo” is optional and denotes a slightly more formal
+	tone, while its omission denotes more of an imperative command. In cases where the use of “wo” is
+	optional, it is indicated by parentheses. Many times you will see the character of は (“ha”)
+	written, but if it is used outside of a word it is pronounced “wa” like the character わ. In
+	addition, if the character へ is used in a word it is pronounced “hey”, while if it is used
+	outside of a word in a sentence it is pronounced “eh” identical to え.
+</p>
+<p>
+	Many written words in common usage drop the unemphasized and sometimes silent “u” う. Two examples
+	are “bougu” (bogu) and “kyuu” (kyu). Sometimes it is easier for nonJapanese speakers to think of
+	these “silent” characters as a sign to hold the vowel or consonant a little longer.
+</p>
+<p>
+	For ease of pronunciation and computerized input systems, the phonetic romanization (Hepburn
+	system) is used in this glossary. The complete roman spelling is first, followed by the common
+	spelling in parentheses if different from the phonetic version. The kanji follow the romanized
+	version of the word, and the hiragana phonetic characters are last.
+</p>
+<p>A pronunciation guide follows the glossary if you feel like you need more information.</p>
+
+<h2>A brief note about dialects:</h2>
+<p>
+	There are about one hundred dialects of Japanese, including those that were constructed
+	deliberately. They range from slight differences in pronunciation to differences so pronounced
+	that the dialect is unintelligible for other Japanese speakers from other parts of the country.
+	Please keep this in mind as the terminology from school to school and individual instructors may
+	be different from what is listed here.
+</p>
+
+<h2>A few words about politeness:</h2>
+<p>
+	Japanese has many levels of formality. The ritualized language used within Kendo practice is more
+	formal and polite than everyday language. The general rule is that the longer the phrase, the more
+	polite it is.
+</p>
+<p>
+	As an example, the following phrases all mean “Thank you”, the English translation roughly
+	approximates the level of formality implied by their usage:
+</p>
+<dl>
+	<dt>Doumo (Domo): どうも</dt>
+	<dd>thanks (informal)</dd>
+
+	<dt>Arigato: 有難う【ありがとう】</dt>
+	<dd>thanks (informal)</dd>
+
+	<dt>Doumo arigato: どうも有難う 【どうもありがとう】</dt>
+	<dd>thank you (formal)</dd>
+
+	<dt>Arigato gozaimasu: 有難うご座います【ありがとうございます】</dt>
+	<dd>thank you (formal)</dd>
+
+	<dt>Doumo arigato gozaimasu: どうも有難うご座います【どうもありがとうございます】</dt>
+	<dd>thank you (very much) (formal+)</dd>
+</dl>
+<p>Notice that the phrase used during kendo practice is the “formal+” version.</p>
+
+<h2>Definitions – now the good stuff!</h2>
+
+<h3>People:</h3>
+<dl>
+	<dt>Sensei: 先生 【せんせい】</dt>
+	<dd>teachers, instructors – in kendo usually reserved for someone at 3rd Dan and up</dd>
+
+	<dt>Senpai: 先輩 【せんぱい】</dt>
+	<dd>senior</dd>
+
+	<dt>Kouhai (Kohai): 後輩 【こうはい】</dt>
+	<dd>junior</dd>
+
+	<dt>Kenshi: 剣士 【けんし】</dt>
+	<dd>a practitioner of kendo, fencer</dd>
+
+	<dt>Kendoka: 剣道家【けんどうか】</dt>
+	<dd>a practioner of kendo, fencer</dd>
+
+	<dt>Senshu: 選手 【せんしゅ】</dt>
+	<dd>player, competitor</dd>
+
+	<dt>Shinpan: 審判 【しんぱん】</dt>
+	<dd>referee</dd>
+
+	<dt>Shinpanin: 審判員 【しんぱんいん】</dt>
+	<dd>referee</dd>
+</dl>
+<p>
+	Cultural note: the relationship between senior and junior is one aspect of kendo that establishes
+	not only a hierarchy, but also a support structure. A 6th Dan and a 7th Dan will use these terms
+	and relationships just as a beginner and ranking person might.
+</p>
+
+<h3>Equipment:</h3>
+<dl>
+	<dt>Keikogi: 稽古着【けいこぎ】</dt>
+	<dd>kendo uniform (top)</dd>
+
+	<dt>Hakama: 袴 【はかま】</dt>
+	<dd>man's formal divided skirt (kendo uniform bottom)</dd>
+
+	<dt>Bougu (Bogu): 防具 【ぼうぐ】</dt>
+	<dd>protector (general name for kendo equipment)</dd>
+
+	<dt>Men: 面 【めん】</dt>
+	<dd>face mask</dd>
+
+	<dt>Kote: 小手 【こて】</dt>
+	<dd>gauntlet (used in Kendo)</dd>
+
+	<dt>Dou (Do): 胴 【どう】</dt>
+	<dd>body protector</dd>
+
+	<dt>Tare: 垂れ 【たれ】</dt>
+	<dd>hanging waist protector</dd>
+
+	<dt>Zekken: 【ゼッケン】</dt>
+	<dd>name marker (from the German word ‘Decken’), worn over the center panel of the Tare</dd>
+
+	<dt>Nafuda: 名札 【なふだ】</dt>
+	<dd>name marker</dd>
+
+	<dt>Himo: 紐 【ひも】</dt>
+	<dd>string/lacing used to secure equipment</dd>
+
+	<dt>Shinai: 竹刀 【しない】</dt>
+	<dd>bamboo sword</dd>
+
+	<dt>Bokken: 木剣 【ぼっけん】</dt>
+	<dd>wooden sword</dd>
+
+	<dt>Bokutou (Bokuto): 木刀 【ぼくとう】</dt>
+	<dd>wooden sword</dd>
+
+	<dt>Tenugui: 手ぬぐい【てぬぐい】</dt>
+	<dd>handtowel, used to wrap the head before wearing the Men</dd>
+
+	<dt>Tsuba: つば</dt>
+	<dd>: handguard</dd>
+
+	<dt>Tsubadome: つば止め【つばどめ】</dt>
+	<dd>handguard stopper</dd>
+</dl>
+
+<h3>At the start and end of practice:</h3>
+<dl>
+	<dt>Retsu: 列 【れつ】</dt>
+	<dd>line, or to form a line</dd>
+
+	<dt>Kiwotsuke (pronounced Kyotsuke): 気を付け 【きをつけ】</dt>
+	<dd>(Stand to) attention!</dd>
+
+	<dt>Seiza 正座, 正坐 【せいざ】</dt>
+	<dd>sitting correctly (Japanese-style); sitting with the knees underneath the body</dd>
+
+	<dt>Chakuza 着座 【ちゃくざ】</dt>
+	<dd>Sit in seiza position</dd>
+
+	<dt>Mokusou (Mokuso): 黙想 【もくそう】</dt>
+	<dd>meditation; silent contemplation</dd>
+
+	<dt>Yame: 止め 【やめ】</dt>
+	<dd>stop (command)</dd>
+
+	<dt>Hajime: 始め【はじめ】</dt>
+	<dd>start (command)</dd>
+
+	<dt>Mokusou-yame (Mokuso-yame): 黙想止め【もくそうやめ】</dt>
+	<dd>stop meditating</dd>
+
+	<dt>ni mukat-te: に向かって【にむかって】</dt>
+	<dd>face towards a certain direction (command). Can be abbreviated as just "ni".</dd>
+
+	<dt>Shoumen ni: 正面に 【しょうめんに】</dt>
+	<dd>the front, façade (secular)</dd>
+
+	<dt>Shinzen ni: 神前に 【しんぜんに】</dt>
+	<dd>before god, before an altar (Buddhist)</dd>
+
+	<dt>Kamiza ni: 上座に 【かみざに】</dt>
+	<dd>chief seat; seat of honor</dd>
+
+	<dt>Sensei ni: 先生に 【せんせいに】</dt>
+	<dd>teacher</dd>
+
+	<dt>Sensei gata ni: 先生方に 【せんせいがたに】</dt>
+	<dd>teachers</dd>
+
+	<dt>Otagai ni: お互いに【おたがいに】</dt>
+	<dd>each other, reciprocal</dd>
+
+	<dt>Rei: 礼【れい】</dt>
+	<dd>Bow</dd>
+
+	<dt>Yoroshiku onegai shimasu: 宜しくお願いします【よろしくおねがいします】</dt>
+	<dd>
+		a phrase that does not directly translate into English. "onegai" generally means "to ask for" or
+		"please". "Yoroshiku" is a phrase used when you receive some sort of accommodation from another
+		party, and that you are acknowledging this imposition by requesting the other party's good will
+		and understanding. This phrase can also be used as a greeting, expressing these same sentiments.
+		Can be abbreviated to “onegai shimasu”.
+	</dd>
+
+	<dt>Doumo arigato gozaimashita : どうも有難うご座いました【どうもありがとうご ざいました】</dt>
+	<dd>thank you very much (past tense) (formal+)</dd>
+
+	<dt>Men (wo) tsuke: 面(を)付け【めん(を)つけ】</dt>
+	<dd>put on your men</dd>
+
+	<dt>Men (wo) tore: 面(を)取れ【めん(を)とれ】</dt>
+	<dd>take off your men</dd>
+
+	<dt>Shinai (wo) mote: 竹刀(を)持て【しない(を)もて】</dt>
+	<dd>pick up your shinai</dd>
+</dl>
+
+<h3>Numbers: These are the cardinal numbers.</h3>
+<dl>
+	<dt>Ichi: 一 【いち】</dt>
+	<dd>one</dd>
+
+	<dt>Ni: 二 【に】</dt>
+	<dd>two</dd>
+
+	<dt>San: 三 【さん】</dt>
+	<dd>three</dd>
+
+	<dt>Shi (Yon): 四 【し; よん】</dt>
+	<dd>four</dd>
+
+	<dt>Go: 五 【ご】</dt>
+	<dd>five</dd>
+
+	<dt>Roku : 六【ろく】</dt>
+	<dd>six</dd>
+
+	<dt>Shichi (Nana): 七 【しち, なな】</dt>
+	<dd>seven</dd>
+
+	<dt>Hachi: 八【はち】</dt>
+	<dd>eight</dd>
+
+	<dt>Kyu (Ku): 九 【きゅう; く】</dt>
+	<dd>nine</dd>
+
+	<dt>Jyu: 十 【じゅう】</dt>
+	<dd>ten</dd>
+
+	<dt>Ni-Jyu: 二十【にじゅう】</dt>
+	<dd>twenty</dd>
+
+	<dt>San-Jyu: 三十【さんじゅう】</dt>
+	<dd>thirty</dd>
+
+	<dt>Yon-Jyu</dt>
+	<dd>forty</dd>
+
+	<dt>Go-Jyu</dt>
+	<dd>fifty</dd>
+
+	<br />
+
+	<dt>[number] kai: 回 【かい】</dt>
+	<dd>number of times (postpositional counter for occurrences). Ex. San-Jyu kai: Thirty times</dd>
+
+	<dt>[number] dan: 段【だん】</dt>
+	<dd>
+		rank (postpositional counter for “black belt” ranks). Exception: Shodan 初段 【しょだん】: 1st
+		degree “black belt”
+	</dd>
+
+	<dt>[number] kyuu (kyu): 級 【きゅう】</dt>
+	<dd>rank (postpositional counter for non black belt ranks)</dd>
+
+	<dt>Ippon: 一本【いっぽん】</dt>
+	<dd>a point, a (scoring) blow</dd>
+</dl>
+
+<h3>Warm up exercises:</h3>
+<dl>
+	<dt>Taisou (Taiso): 体操 【たいそう】</dt>
+	<dd>exercise, warm up exercises</dd>
+
+	<dt>Suburi: 素振り 【すぶり】</dt>
+	<dd>
+		a cut or stretching movement with a (wooden/bamboo) sword, usually from a starting position
+		above one's head.
+	</dd>
+
+	<dt>Jyouge suburi (Jyoge suburi) 上下素振り 【じょうげすぶり】</dt>
+	<dd>high to low, up and down cuts</dd>
+
+	<dt>Renzoku sayuu suburi: 連続左右素振り【れんぞくさゆうすぶり】</dt>
+	<dd>
+		continuous left and right. Often abbreviated as “sayuu-suburi” 左右素振り【さ ゆうすぶり】
+	</dd>
+
+	<dt>Katate suburi: 片手素振り 【かたてすぶり】</dt>
+	<dd>one handed</dd>
+
+	<dt>Yoko-men suburi: 横面素振り【よこめんすぶり】</dt>
+	<dd>to the sides of the men</dd>
+
+	<dt>Matawari suburi: 股割り素振り【またわりすぶり】</dt>
+	<dd>
+		(lit. thigh splitting cut). Feet placed outside of shoulder width, with toes pointing at a 45
+		degree angle. This stretch starts as a big men cut and finishes at kote height, while squatting
+		during the downward cut. An upright posture thoughout the stretch is key.
+	</dd>
+</dl>
+
+<h3>Ashi-sabaki Footwork:</h3>
+<dl>
+	<dt>Okuri-ashi: 送り足【おくりあし】</dt>
+	<dd>sliding step with the right foot forward</dd>
+
+	<dt>Hiraki-ashi: 開き足【ひらきあし】</dt>
+	<dd>“opening” side step, used in sayuu suburi</dd>
+
+	<dt>Ayumi ashi: 歩み足【あゆみあし】</dt>
+	<dd>walking step, such as the step used by the receiver of kirikaeshi</dd>
+
+	<dt>Fumikomi: 踏み込み 【ふみこみ】</dt>
+	<dd>the lunge step which accompanies a strike, lateral “stomp”</dd>
+</dl>
+
+<h3>Directions:</h3>
+<dl>
+	<dt>Mae: 前【まえ, まい】</dt>
+	<dd>forward</dd>
+
+	<dt>Ato: 後 【あと】</dt>
+	<dd>backward</dd>
+
+	<dt>Hidari: 左 【ひだり】</dt>
+	<dd>left</dd>
+
+	<dt>Migi: 右 【みぎ】</dt>
+	<dd>right</dd>
+
+	<dt>Susume: 進め 【すすめ】</dt>
+	<dd>step forward, advance (command)</dd>
+
+	<dt>Sagare: 下がれ【さがれ】</dt>
+	<dd>step backward (command)</dd>
+</dl>
+
+<h3>Practice terms:</h3>
+<dl>
+	<dt>Shiai: 試合 【しあい】</dt>
+	<dd>competition</dd>
+
+	<dt>Waza: 業 【わざ】</dt>
+	<dd>technique</dd>
+
+	<dt>Kihon waza: 基本業【きほんわざ】</dt>
+	<dd>basic techniques</dd>
+
+	<dt>Sashi-men: 差し面【さしめん】</dt>
+	<dd>Fast or quick-men</dd>
+
+	<dt>Tai-tou (Tai-to): 帯刀 【たいとう】</dt>
+	<dd>
+		Sword position at the level of the belt/obi (command) after the bow and before initiating the
+		approach to sonkyo
+	</dd>
+
+	<dt>Sonkyo: 蹲踞 【そんきょ】</dt>
+	<dd>formal crouch at the beginning/ending of a kendo match</dd>
+
+	<dt>Sage-tou (Sage-to) : 下げ刀【さげとう】</dt>
+	<dd>Lowered sword position before and after Tai-tou</dd>
+
+	<dt>Kamae: 構え 【かまえ】</dt>
+	<dd>To make ready (“on guard”)</dd>
+
+	<dt>Jodan no Kamae : 上段の構え【じょうだんのかまえ】</dt>
+	<dd>
+		High position of the sword over your head. Considered a very aggressive stance for higher level
+		Kendo practioners.
+	</dd>
+
+	<dt>Chudan no Kamae : 中段の構え【ちゅうだんのかまえ】</dt>
+	<dd>Most common mid level stance with tip of sword pointing at your opponents throat.</dd>
+
+	<dt>Gedan no Kamae : 下段の構え【げだんのかまえ】</dt>
+	<dd>Least common stance. Sword tip is below kneecap level.</dd>
+
+	<dt>Kirikaeshi: 切り返し 【きりかえし】</dt>
+	<dd>
+		(lit. returning cuts) An exercise where a certain sequence of men-suburi is traded between two
+		partners, usually at the beginning and end of practice.
+	</dd>
+
+	<dt></dt>
+</dl>
+
+<style>
+	dt {
+		font-weight: 500;
+	}
+	dd {
+		color: var(--pico-secondary);
+	}
+</style>
