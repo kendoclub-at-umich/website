3734690bcd9caec74c892367be0eef3113290c70
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-08-06 16:50:57 -0400

    feat: :sparkles: add html page and redirect to it

---
+++ src/routes/resources/parts-of-a-shinai-and-how-to-hold-it/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Parts of a Shinai and How to Hold It | Kendo Club at the University of Michigan</title>
+	<meta
+		name="description"
+		content="Includes labelled diagrams of the bamboo swords used in Kendo and demonstrates how to hold one properly using pictures from multiple angles."
+	/>
+</svelte:head>
+
+<h1>Parts of a Shinai and How to Hold It</h1>
