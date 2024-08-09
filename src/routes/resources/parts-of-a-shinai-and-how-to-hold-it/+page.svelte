66aa860c2558de521b52edda0815c430e2bd6a6f
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-08-09 16:06:50 -0400

    feat: :sparkles: add page content and use float for image placement

--- src/routes/resources/parts-of-a-shinai-and-how-to-hold-it/+page.svelte
+++ src/routes/resources/parts-of-a-shinai-and-how-to-hold-it/+page.svelte
@@ -7,3 +7,253 @@
 </svelte:head>
 
 <h1>Parts of a Shinai and How to Hold It</h1>
+
+<h2>Part 1:</h2>
+
+<figure class="float-left">
+	<enhanced:img
+		src="./deconstructed-shinai.jpg"
+		sizes="min(100px, 100vw)"
+		alt="In black and white, parts of a shinai laid out on the floor."
+	/>
+	<figcaption>A deconstructed shinai</figcaption>
+</figure>
+
+<p>
+	Pictured to the left you will see an exploded version of the typical shinai. It is generally
+	comprised of: 4 slats of bamboo, leather cap, plastic plug, thin leather strap, handle, a very
+	small square of very thin steel, twine, cross guard and the cross guard support. As you can tell
+	from the age of the picture used, the overall design and components of the shinai have not changed
+	dramatically over the years. Although familiarity with the shinai is not required for promotion
+	testing, it is a very important safety factor in your Kendo practice. We request that every member
+	take the time to become familiar with how to take apart, adjust and reassemble the shinai. With
+	the ability to deconstruct the shinai, faulty or damaged pieces can be repaired or replaced. A
+	damaged shinai is not only a bad reflection of you the Kendoist, but also a potentially serious
+	threat to your fellow sempei and kohai. Ask one of your sempei how to take it apart and inspect
+	the pieces. You will eventually want to take the shinai apart so that you can adjust the cord as
+	the leather handle begins to stretch. This is also an excellent opportunity to adjust the handle
+	length if needed.
+</p>
+<div class="clear-float"></div>
+<p>
+	Pictured below is a back view and a profile view of a typical shinai. The correct names of each of
+	the red points are given in English to the right. Becoming familiar with these terms. Having a
+	common reference for discussion greatly reduces confusion in conversation …
+</p>
+
+<figure class="float-left">
+	<enhanced:img
+		src="./shinai-diagram-japanese-labels.jpg"
+		sizes="min(100px, 100vw)"
+		alt="Diagram of two shinai labelled in japanese."
+	/>
+	<figcaption>
+		First shinai has its "striking side" facing down. Second shinai has it facing left.
+	</figcaption>
+</figure>
+
+<p>Kissaki – this is the thrusting surface of the shinai, also the tip of the sword.</p>
+<p>Sakigawa – the name of the leather cap that covers the tip.</p>
+<p>
+	Monouchi – this is the valid striking area of the shinai. It starts at the very tip of the sword
+	and goes to the top most edge of the Nakayui. Note: since the shinai only has a single blade, this
+	region only applies to the face of the shinai. You may not achieve a point hitting someone with
+	the side of the shinai, even if it is in the Monouchi.
+</p>
+<p>
+	Nakayui – the narrow leather strap, used to indicate the lowest portion of your shinai’s
+	striking/cutting surface.
+</p>
+<p>
+	Chikama – Any point below the Nakayui to the Tuba which is not a valid scoring surface. Tsuru –
+	the twine or strong cord that denotes the back of the shinai. This string also holds most of the
+	major components in place.
+</p>
+<p>
+	Tsuba – this is the cross guard or hand guard of the shinai, unlike western style guards this is
+	generally round and small.
+</p>
+<p>
+	Tsubadome – this is the supporting piece that holds the tsuba in place so it does not travel
+	during use.
+</p>
+<p>Tsuka – is the name of the handle, generally leather, that acts as the grip for the shinai.</p>
+<p>
+	Tsukagashira – is the name of the end of the shinai, generally referred to as a pommel. The bottom
+	edge of your left hand wraps around here while holding the shinai.
+</p>
+
+<div class="clear-float"></div>
+
+<h2>PART 2: How to correctly hold the shinai.</h2>
+
+<div class="double-floated">
+	<figure class="float-left">
+		<enhanced:img
+			src="./arm-position-front-view.jpg"
+			sizes="min(100px, 100vw)"
+			alt="Woman in kendo uniform facing forward, demonstrating arm position."
+		/>
+	</figure>
+
+	<figure class="float-right">
+		<enhanced:img
+			src="./arm-position-side-view.jpg"
+			sizes="min(100px, 100vw)"
+			alt="Woman in kendo uniform facing right, demonstrating arm position."
+		/>
+	</figure>
+</div>
+
+<p>
+	Lets begin this portion of the discussion with an exercise. Stand up with your hands at your belt
+	level. Place your open hands together as if you were going to pray. Fingertips are all together.
+	Your arms are comfortably relaxed and naturally bent at the elbows. Keep your shoulders and elbows
+	relaxed like the pictures to the left and right.
+</p>
+<div class="clear-float"></div>
+
+<div class="double-floated">
+	<figure class="float-left">
+		<enhanced:img
+			src="./arm-position-side-view-hands-apart.jpg"
+			sizes="min(100px, 100vw)"
+			alt="Woman in kendo uniform facing right, demonstrating arm position. Hands apart showing where to place on a shinai."
+		/>
+	</figure>
+
+	<figure class="float-right">
+		<enhanced:img
+			src="./left-hand-shinai-grip.jpg"
+			sizes="min(100px, 100vw)"
+			alt="Close up of left hand correctly holding the shinai handle at the very bottom. Nails are painted a pearly pink."
+		/>
+	</figure>
+</div>
+
+<p>
+	Now move you right hand ahead of your left hand by an imaged fist distance between them. The
+	shinai goes into the left hand as show on the right. This positioning is very important.
+</p>
+
+<div class="clear-float"></div>
+<p>
+	Note how the left hand comes over the edge slightly and that the handle is actually at an angle in
+	the hand. We do not hold a shinai as you would a flashlight or a baseball bat. The angle allows us
+	to strike at the top of our opponents Men and also to have greater range in our attack. Also
+	notice that the hand is not tightly clenched, only the final two fingers are exerting pressure on
+	the tsuka/handle.
+</p>
+
+<figure class="float-left">
+	<enhanced:img
+		src="./right-hand-shinai-grip-and-mistakes.jpg"
+		sizes="min(100px, 100vw)"
+		alt="Two examples of right hand grip, one correct and one incorrect, each shown with and without a shinai."
+	/>
+</figure>
+
+<p>
+	The left hand grip is displayed here. The picture is from the other side for clarity. Correct
+	positioning is marked with an “O” where common mistakes are marked with the “X”. As with the left
+	hand, the right hand is not clenched tightly either. Notice in the second picture where the
+	fingers are noted to be relaxed, but not open.
+</p>
+
+<div class="clear-float"></div>
+
+<figure class="float-right">
+	<enhanced:img
+		src="./both-hands-shinai-grip-top-view.jpg"
+		sizes="min(100px, 100vw)"
+		alt="View from above of gripping a shinai, as if looking down at one's own hands. Dotted lines show a V shape made between the thumb and index finger of each hands, and the V shapes lining up along the shinai handle."
+	/>
+</figure>
+
+<p>
+	At any point during cutting, you should be able to open your fingers and they should be pointing
+	directly away from you at every moment in cutting. Looking top down you hands should look like
+	this.
+</p>
+
+<p>
+	In bogu it is difficult to see correct placement of the hands, because of the kote. But your cuts
+	will reflect to everyone else if you positioning is incorrect. It is important to always be aware
+	of correct grip on the shinai. Check yourself, until it becomes second nature to you.
+</p>
+
+<div class="clear-float"></div>
+
+<figure class="float-left">
+	<enhanced:img
+		src="./both-hands-shinai-grip-mistakes.jpg"
+		sizes="min(100px, 100vw)"
+		alt="Three examples of incorrect shinai grips, two from above and one from the side. First and second examples show baseball bat grips. Third example shows left hand a few inches up from the end of shinai handle."
+	/>
+</figure>
+
+<p>
+	<strong>
+		Make every effort to avoid gripping the shinai as shown to the left. They are all excellent
+		examples of what not to do.
+	</strong>
+	When gripping the shinai correctly you will naturally develop a strong kamae and be much less open
+	to attacks. Now that the hands are gripping the shinai properly, let’s focus on the final aspect of
+	holding the shinai. The tip of the shinai or Kissaki is always pointed toward your opponent’s throat.
+	If the two of you were to walk directly at each other, when you met you should both have the kissaki
+	touching each others throat. When practicing alone, picture a perfect reflection of yourself as your
+	target and adjust the height of the tip of the shinai accordingly.
+</p>
+
+<div class="clear-float"></div>
+
+<div class="inline">
+	<figure>
+		<enhanced:img
+			src="./both-hands-shinai-grip-side-view.jpg"
+			sizes="min(100px, 100vw)"
+			alt="Woman in kendo uniform facing right, holding a shinai correctly. Left hand is at the bottom of the shinai handle. Right hand is directly under the hand guard."
+		/>
+	</figure>
+	<figure class="float-right">
+		<enhanced:img
+			src="./both-hands-shinai-grip-in-bogu-front-view.jpg"
+			sizes="min(100px, 100vw)"
+			alt="Kendo practitioner in full armor, holding a shinai and facing forward. Demonstrates the angle of the shinai and where the tip points."
+		/>
+	</figure>
+</div>
+
+<div class="clear-float"></div>
+<p>
+	If you have any further questions, feel free to address them to your sempei or email them to us.
+</p>
+
+<style>
+	figure {
+		margin-bottom: 15px;
+		display: table;
+		max-width: 47%;
+	}
+	figcaption {
+		display: table-caption;
+		caption-side: bottom;
+	}
+	figure.float-left {
+		float: left;
+		margin-right: 15px;
+	}
+	figure.float-right {
+		float: right;
+		margin-left: 15px;
+	}
+	div.clear-float {
+		clear: both;
+	}
+	div.inline figure {
+		display: inline-block;
+	}
+	div.double-floated figure {
+		max-width: 35%;
+	}
+</style>
