8139ef0b79239fbf3135f0411d130ce0254d3aa7
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-08-09 12:26:30 -0400

    feat: :sparkles: improve word choice in meta description

--- src/routes/resources/parts-of-a-shinai-and-how-to-hold-it/+page.svelte
+++ src/routes/resources/parts-of-a-shinai-and-how-to-hold-it/+page.svelte
@@ -2,7 +2,7 @@
 	<title>Parts of a Shinai and How to Hold It | Kendo Club at the University of Michigan</title>
 	<meta
 		name="description"
-		content="Includes labelled diagrams of the bamboo swords used in Kendo and demonstrates how to hold one properly using pictures from multiple angles."
+		content="Includes labelled diagrams of the bamboo swords used in Kendo and demonstrates how to hold one properly with pictures from multiple angles."
 	/>
 </svelte:head>
 
