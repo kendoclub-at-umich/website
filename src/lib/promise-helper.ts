a0fb3f44b9088af7c403cc6c9fba345642a04e92
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-08-25 16:56:53 -0400

    style: 🎨 seperate nested anonomous function into multiple lines to improve readablity

--- src/lib/promise-helper.ts
+++ src/lib/promise-helper.ts
@@ -1,3 +1,5 @@
 export function wait(milliseconds: number): Promise<void> {
-	return new Promise((resolve) => setTimeout(() => resolve(), milliseconds));
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(), milliseconds);
+	});
 }
