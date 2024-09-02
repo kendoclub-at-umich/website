d6a3d10a86d22f45d5eb3bc97839b2e576c6edc8
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-09-01 21:54:58 -0400

    ci: 👷 add configure type checking for typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -8,23 +8,26 @@ import globals from 'globals';
 
 export default ts.config(
 	js.configs.recommended,
-	...ts.configs.strict,
+	...ts.configs.strictTypeChecked,
 	...svelte.configs['flat/recommended'],
 	prettier,
 	...svelte.configs['flat/prettier'],
 	{
-		rules: {
-			eqeqeq: ['warn', 'smart'],
-			'svelte/block-lang': ['error', { script: 'ts' }],
-			'svelte/spaced-html-comment': 'warn'
-		}
-	},
-	{
 		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+				extraFileExtensions: ['.svelte']
+			},
 			globals: {
 				...globals.browser,
 				...globals.node
 			}
+		},
+		rules: {
+			eqeqeq: ['warn', 'smart'],
+			'svelte/block-lang': ['error', { script: 'ts' }],
+			'svelte/spaced-html-comment': 'warn'
 		}
 	},
 	{
@@ -34,6 +37,10 @@ export default ts.config(
 				parser: ts.parser
 			}
 		}
+	},
+	{
+		files: ['*.js', '*.cjs'],
+		...ts.configs.disableTypeChecked
 	},
 	{
 		ignores: ['build/', '.svelte-kit/', 'dist/', '.netlify/']
